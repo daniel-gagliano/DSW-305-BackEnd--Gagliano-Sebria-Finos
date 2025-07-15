2815174c2a7251f590ebd83d536f1d609b47ed24
ParedDeLana <sebriatomas@gmail.com>
2025-07-15 20:34:38 -0300

    CRUD Descuento

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const categoriaController = require('./categoriaController');
 const articuloController = require('./articuloController');
 const localidadRouter = require('./localidadController');
 const provinciaRouter = require('./provinciaController');
+const descuentoRouter = require('./descuentoController');
 const cors = require('cors');
 const app = express();
 
@@ -16,6 +17,7 @@ app.use('/categorias', categoriaController);
 app.use('/articulos', articuloController);
 app.use('/localidades', localidadRouter);
 app.use('/provincias', provinciaRouter);
+app.use('/descuentos', descuentoRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ migrations/20250715233025_descuento_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `Descuento` (
+    `cod_descuento` INTEGER NOT NULL AUTO_INCREMENT,
+    `desc_descuento` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`cod_descuento`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
